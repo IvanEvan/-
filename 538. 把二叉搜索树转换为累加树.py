647f952d29d50ddf72d58fec39a2e9929a68e4dd
Evan <ivanyang081539@gmail.com>
2020-09-22 10:19:38 +0800

    Update 538. 把二叉搜索树转换为累加树.py

--- 538. 把二叉搜索树转换为累加树.py
+++ 538. 把二叉搜索树转换为累加树.py
@@ -9,10 +9,10 @@ class Solution:
     def convertBST(self, root: TreeNode) -> TreeNode:
         if root:
             max_node = root
-            while max_node.right:
+            while max_node.right:  # 先找到最右的最大值节点
                 max_node = max_node.right
 
-            self.helper(root, max_node)
+            self.helper(root, max_node)  # 递归 反向中序 从大到小排序
 
             return root
         else:
@@ -23,13 +23,13 @@ class Solution:
         if not root:
             return None
 
-        else:
-            self.helper(root.right, max_root)
+        else:  # 反向中序遍历
+            self.helper(root.right, max_root)  
 
             if root == max_root:
-                max_val = max_root.val
+                max_val = max_root.val  # 取出最大值节点对应的值
             else:
-                root.val += max_val
+                root.val += max_val  # 累加节点值
                 max_val = root.val
 
             self.helper(root.left, max_root)
